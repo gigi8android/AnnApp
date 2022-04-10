74c555f82e76b2af02d90ccfd6bce04cc6547811
GiGi <92906443+gigi8android@users.noreply.github.com>
2022-04-11 00:06:13 +1000

    Update dcpython.py

--- dcpython.py
+++ dcpython.py
@@ -1,5 +1,5 @@
-from boto.s3.connection import S3Connection
-s3 = S3Connection(os.environ['S3_KEY'], os.environ['S3_SECRET'])
+import os
+api_key = os.getenv("API_KEY", "optional-default")
 
 import pandas as pd
 import plotly.express as px
